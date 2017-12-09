43233e95b79e774eee5ffe4ad7af71f08b3fb8c5
arun.mathew <arunkmathewulikkal@gmail.com>
2017-12-09 13:19:00 +0530

    complete CRUD operation using memory stubing (not using DB)

--- messenger-api/src/main/java/com/arun/rest/study/messenger_api/controller/MessageController.java
+++ messenger-api/src/main/java/com/arun/rest/study/messenger_api/controller/MessageController.java
@@ -3,8 +3,11 @@ package com.arun.rest.study.messenger_api.controller;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +17,41 @@ import com.arun.rest.study.messenger_api.model.Message;
 import com.arun.rest.study.messenger_api.service.MessangerService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageController {
 	
 	MessangerService messangerService = new MessangerService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessage()
 	{
 		return messangerService.getAllMessages();
 	}
 	
 	@GET
-	@Path("{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMassage(@PathParam("messageId") long id)
+	@Path("/{messageId}")
+		public Message getMassage(@PathParam("messageId") long id)
 	{
 		return messangerService.getMessage(id);
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	public Message addMessage( Message message) {
+	public Message addMessage(Message message) {
 		return messangerService.addMessage(message);
 	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message){
+		message.setId(messageId);
+		return messangerService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId") long messageId) {
+		return messangerService.deleteMessage(messageId);
+	}
+	
 }
